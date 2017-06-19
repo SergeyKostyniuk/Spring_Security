d80613040a48ea37671b63d829845229be85c037
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-19 14:09:56 +0300

    add MainController

--- src/main/java/ua/sergey/test/web/MainController.java
+++ src/main/java/ua/sergey/test/web/MainController.java
@@ -1,4 +1,13 @@
 package ua.sergey.test.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+    @RequestMapping("/")
+    public String getMainPage() {
+        return "index";
+    }
+
 }
