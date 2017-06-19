ddd615b1b28a759deaf5eeaf6221ca36bef537a0
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-19 19:14:13 +0300

    add User

---
+++ src/main/java/ua/sergey/test/domain/User.java
@@ -0,0 +1,20 @@
+package ua.sergey.test.domain;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+    private List<GrantedAuthority> authorities;
+    private String password;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+
+}
