dd7fd751df1050cd9ff11ccb5061373b796fecd7
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-19 14:21:31 +0300

    properties to yaml

--- src/main/java/ua/sergey/test/Application.java
+++ src/main/java/ua/sergey/test/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws  Exception {
