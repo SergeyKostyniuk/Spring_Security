37334471993c011a24ddea0d9caa7f581aa75e3d
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-20 03:16:23 +0300

    change HTML files

--- src/main/java/ua/sergey/test/web/MainController.java
+++ src/main/java/ua/sergey/test/web/MainController.java
@@ -1,7 +1,9 @@
 package ua.sergey.test.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -10,4 +12,12 @@ public class MainController {
         return "index";
     }
 
+    public String getLogin(@RequestParam(value ="error", required = false)String error,
+                           @RequestParam(value ="logout", required = false)String logout,
+                           Model model) {
+        model.addAttribute("error", error !=null);
+        model.addAttribute("logout", error !=null);
+        return "login";
+
+    }
 }
